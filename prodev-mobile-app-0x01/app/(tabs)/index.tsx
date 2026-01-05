7029d3327ee68a102ece1cd737e560e1795c05f0
Reseanne Rampou <reseanner@gmail.com>
2026-01-05 22:47:10 +0200

    Update index.tsx

--- prodev-mobile-app-0x01/app/(tabs)/index.tsx
+++ prodev-mobile-app-0x01/app/(tabs)/index.tsx
@@ -5,16 +5,13 @@ export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text>Entry Screen - Awesome</Text>
-
       <View>
         <Text style={styles.largeText}>
-          Typescript is great if you practice more
+          TYPESCRIPT IS GREAT IF YOU PRACTICE MORE
         </Text>
-
         <Text style={styles.mediumText}>
-          React Native provides you a single codebase for cross platforms
+          React Native provides you a single code base for cross platforms
         </Text>
-
         <Text style={styles.smallText}>
           ALX is awesome
         </Text>
@@ -31,7 +28,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     padding: 20,
   },
-
   largeText: {
     fontSize: 30,
     color: "#f44336",
@@ -39,7 +35,6 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     fontVariant: ["small-caps"],
   },
-
   mediumText: {
     fontSize: 20,
     color: "         
@@ -47,7 +42,6 @@ const styles = StyleSheet.create({
     fontWeight: "500",
     textAlign: "right",
   },
-
   smallText: {
     fontSize: 15,
     color: "#2196f3",
