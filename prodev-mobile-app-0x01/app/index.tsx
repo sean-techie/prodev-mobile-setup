b0e2a4a1d49e94d5c4ea50308e390879b37ee641
Reseanne Rampou <reseanner@gmail.com>
2026-01-05 23:08:52 +0200

    Update index.tsx

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { Stack } from 'expo-router';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function RootLayout() {
+export default function App() {
   return (
-    <SafeAreaProvider>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-    </SafeAreaProvider>
+    <View style={styles.container}>
+      <Text style={styles.largeText}>Entry Screen - Awesome</Text>
+      <Text style={styles.mediumText}>Welcome to your first app!</Text>
+      <Text style={styles.smallText}>Start building your app here.</Text>
+    </View>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
